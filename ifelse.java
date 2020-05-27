3070c5e01cb25d7c584a6a7beeb76249498423e9
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-05-27 16:58:58 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+class IfStatement {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        // checks if number is greater than 0
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+        System.out.println("This statement is always executed.");
+    }
+}
