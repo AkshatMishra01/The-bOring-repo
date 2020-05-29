677894eee72e3a69a0a1adf03ae3f35e0c326d70
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-05-29 17:15:55 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,38 @@
+class Main {
+    public static void main(String[] args) {
+
+        int week = 4;
+        String day;
+
+        // switch statement to check day
+        switch (week) {
+            case 1:
+                day = "Sunday";
+                break;
+            case 2:
+                day = "Monday";
+                break;
+            case 3:
+                day = "Tuesday";
+                break;
+
+            // match the value of week
+            case 4:
+                day = "Wednesday";
+                break;
+            case 5:
+                day = "Thursday";
+                break;
+            case 6:
+                day = "Friday";
+                break;
+            case 7:
+                day = "Saturday";
+                break;
+            default:
+                day = "Invalid day";
+                break;
+        }
+        System.out.println("The day is " + day);
+    }
+}
