c3eb8f62d3e8b8761aabac5e94ab8eb1a0372331
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-01 18:05:06 +0530

    static local variable in c++

---
+++ static.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void test()
+{
+    // var is a static variable
+    static int var = 0;
+    ++var;
+
+    cout << var << endl;
+}
+
+int main()
+{
+    
+    test();
+    test();
+
+    return 0;
+}
