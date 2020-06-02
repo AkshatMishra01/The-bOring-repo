3bdc2d730fa0449fd4f2c9469b62f1e7af6bc51c
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-02 08:51:49 +0530

    multidimensional array in c++

---
+++ multi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test[3][2] =
+    {
+        {2, -5},
+        {4, 0},
+        {9, 1}
+    };
+
+    // Accessing two dimensional array using
+    // nested for loops
+    for(int i = 0; i < 3; ++i)
+    {
+        for(int j = 0; j < 2; ++j)
+        {
+            cout<< "test[" << i << "][" << j << "] = " << test[i][j] << endl;
+        }
+    }
+
+    return 0;
+}
