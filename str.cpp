6334414e56d12653463492c550403ed801d0a3f6
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-03 08:27:58 +0530

    Update str.cpp

--- str.cpp
+++ str.cpp
@@ -10,3 +10,17 @@ int main()
     cout << "You entered: " << str << endl;
     return 0;
 }
+// 2nd method of handling strings in c++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declaring a string object
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "You entered: " << str << endl;
+    return 0;
+}
