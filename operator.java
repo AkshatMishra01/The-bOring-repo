18182cdd1934d2ef1827979b63000967c047ffb1
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-04 11:33:52 +0530

    Update operator.java

--- operator.java
+++ operator.java
@@ -1,3 +1,4 @@
+//Assignment operators in java.
 class AssignmentOperator {
     public static void main(String[] args) {
     	
@@ -11,3 +12,31 @@ class AssignmentOperator {
         number2 = number1;
         System.out.println(number2);
     }
+    //Arthmetic operators in java
+    
+class ArithmeticOperator {
+    public static void main(String[] args) {
+    	
+        double number1 = 12.5, number2 = 3.5, result;
+    	
+        // Using addition operator
+        result = number1 + number2;
+        System.out.println("number1 + number2 = " + result);
+    	
+        // Using subtraction operator
+        result = number1 - number2;
+        System.out.println("number1 - number2 = " + result);
+    	
+        // Using multiplication operator
+        result = number1 * number2;
+        System.out.println("number1 * number2 = " + result);
+
+        // Using division operator
+        result = number1 / number2;
+        System.out.println("number1 / number2 = " + result);
+    	
+        // Using remainder operator
+        result = number1 % number2;
+        System.out.println("number1 % number2 = " + result);
+    }
+}
