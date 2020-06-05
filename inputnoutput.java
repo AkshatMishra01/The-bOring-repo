cf33185bd0afabb0382007640d1e3382abb42c48
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-05 10:04:04 +0530

    Update inputnoutput.java

--- inputnoutput.java
+++ inputnoutput.java
@@ -5,3 +5,19 @@ class AssignmentOperator {
         System.out.println("Java programming is interesting.");   
     }
 }
+//input an integer from a user, (INUT SYNTAX IN JAVA)
+import java.util.Scanner;
+
+class Input {
+    public static void main(String[] args) {
+    	
+        Scanner input = new Scanner(System.in);
+    	
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("You entered " + number);
+
+        // closing the scanner object
+        input.close();
+    }
+}
