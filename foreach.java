e549d3aace7a9543ddc9cf3eacd71c9b97fc940d
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-06 14:33:40 +0530

    for each loop in java

---
+++ foreach.java
@@ -0,0 +1,13 @@
+class EnhancedForLoop {
+    public static void main(String[] args) {
+      
+        int[] numbers = {3, 4, 5, -5, 0, 12};
+        int sum = 0;
+      
+        for (int number: numbers) {
+            sum += number;
+        }
+      
+        System.out.println("Sum = " + sum);
+    }
+}
