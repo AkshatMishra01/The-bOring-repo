91fa2580ecb46650c7776a892052251359451140
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-08 10:38:34 +0530

    working of for loop in java

---
+++ forloop.java
@@ -0,0 +1,10 @@
+// Program to print a sentence 10 times
+
+class Loop {
+    public static void main(String[] args) {
+      
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println("Line " + i);
+        }
+    }
+}
