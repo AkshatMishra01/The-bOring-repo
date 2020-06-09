390eee329a71f49f3a92736ada0b1f9bf35372f3
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-09 11:00:52 +0530

    Create comment.java

---
+++ comment.java
@@ -0,0 +1,9 @@
+// "Hello, World!" program example
+ 
+class Main {
+    public static void main(String[] args) {    	
+    {
+        // prints "Hello, World!"
+        System.out.println("Hello, World!");
+    }
+}
