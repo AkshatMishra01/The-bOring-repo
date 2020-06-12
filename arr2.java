f7b300617d7d55ea8bf8302243f7f54d33d7b066
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-13 00:22:38 +0530

    initializing an array during declaration

---
+++ arr2.java
@@ -0,0 +1,12 @@
+class ArrayExample {
+    public static void main(String[] args) {
+
+        // create an array 
+        int[] age = {12, 4, 5, 2, 5};
+
+        // access elements of tha arau
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Element at index " + i +": " + age[i]);
+        }
+    }
+}
