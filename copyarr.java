2474d0d88c87b90efefbe30cb5c5e7a1fd69fde4
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-17 10:02:43 +0530

    all the methods to copy an array in java

--- copyarr.java
+++ copyarr.java
@@ -64,3 +64,20 @@ class Main
         System.out.println(Arrays.toString(integers));
     }
 }
+// To use toString() and copyOfRange() method
+import java.util.Arrays;
+
+class ArraysCopy {
+    public static void main(String[] args) {
+      
+        int[] source = {2, 3, 12, 4, 12, -2};
+      
+        // copying entire source array to destination
+        int[] destination1 = Arrays.copyOfRange(source, 0, source.length);      
+        System.out.println("destination1 = " + Arrays.toString(destination1)); 
+      
+        // copying from index 2 to 5 (5 is not included) 
+        int[] destination2 = Arrays.copyOfRange(source, 2, 5); 
+        System.out.println("destination2 = " + Arrays.toString(destination2));   
+    }
+}
