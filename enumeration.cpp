3c7862ed63654232074a1fc49e965539ed1d1d51
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-18 08:00:28 +0530

    Update enumeration.cpp

--- enumeration.cpp
+++ enumeration.cpp
@@ -17,3 +17,14 @@ device= transistor;
   cout<<"The seleted device has the following rating"<<device;
   return 0;
 }
+// BITWISE operators in c++
+#include<iostream>
+using namespace std;
+enum flags{BOLD = 1,ITALICS = 2,UNDERLINE = 3} design;
+int main()
+{
+int design = BOLD | UNDERLINE;
+  cout<<"My flags design are:"<<design;
+  return 0;
+  
+}
