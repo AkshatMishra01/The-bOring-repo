c5a08083c60c137288f761dd16bb7957095246ac
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-18 07:41:58 +0530

    Update enumeration.cpp

--- enumeration.cpp
+++ enumeration.cpp
@@ -7,3 +7,13 @@ days today;
 today = thursday;
 cout<<"The day is"<<today;
 }
+/// working of enumeration in c++ 
+#include<iostream>
+using nmaespace std;
+enum devicerating {diode=3,transistor=4.9,arduino=5,stm_Nucleo=5}device;
+int main()
+{
+device= transistor;
+  cout<<"The seleted device has the following rating"<<device;
+  return 0;
+}
