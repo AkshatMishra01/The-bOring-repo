eef0ad4a914b79bb1595d3b2f530f17f72b807dd
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-18 07:35:11 +0530

    enumeration in c++

---
+++ enumeration.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+enum days {Sunday,monday,tuesday,wednesday,thursday,friday,saturday};
+int main()
+{
+days today;
+today = thursday;
+cout<<"The day is"<<today;
+}
