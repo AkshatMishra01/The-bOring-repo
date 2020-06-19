3f932c06cb6e1b00916d41f758aed2c52ff80989
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-19 08:24:22 +0530

    constructor overloading in c++

---
+++ constructover.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class friendship
+{
+public:
+int shlok;
+int tanisha;
+int himanshu;
+char care = a;
+friendship(char care)
+{
+cout<<"This is the first friendship function";
+cout<<"I have a character grade:"<<care;
+}
+friendship(char help, char collaborate)
+{
+   cout<<"This is the constructor overloading function";
+   cout<<"The 2 outputs that i have are:"<<x<<"and"<<y;
+}
+};
+int main()
+{
+friendship obj1,obj2(x,y);
+cout<<"This is the constructor overloading function";
+obj1.friendship();
+obj2.friendship();
+return 0;
+}
