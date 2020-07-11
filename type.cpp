d07202024c5b51bd8a214f636677111a5d5abd36
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-07-11 09:08:37 +0530

    type casting in java

---
+++ type.cpp
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    // create int type variable
+    int num = 10;
+    System.out.println("The integer value: " + num);
+
+    // convert into double type
+    double data = num;
+    System.out.println("The double value: " + data);
+  }
+}
