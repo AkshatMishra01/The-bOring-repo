e8693e27bd08b07941bed197e28ad382c122261f
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-07-18 12:34:26 +0530

    i love python

---
+++ python.py
@@ -0,0 +1,12 @@
+print("Neural Networks")
+a = 20;
+print(a+8)
+if(a+8>10):
+    b=a+50;
+    print("b =",b)
+    if(b>10):
+        print("I am the best ")
+    '''This is a multiline comment
+i  love python'''
+    
+    
