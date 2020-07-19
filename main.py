4b85895693fef2fdb010710c67a17766955adf0b
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-07-19 09:20:43 +0530

    type casting, string slicing and operating, basics of python

---
+++ main.py
@@ -0,0 +1,20 @@
+print('is it working')
+a = 678
+b = 567.67
+c = "Harry"
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b)
+typeA = type(b)
+print(typeA)
+typeC = type(c)
+print(typeC)
+e="89.98"
+e= float(e)
+b = str(b)
+print(e,b)
+name = "aks67t"
+print(name.strip())
+var = name.replace("67","ha")
+print(var)
