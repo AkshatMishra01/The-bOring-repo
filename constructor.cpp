e4523c231e63bebc518e8386a6f51967dadd5435
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-01-22 12:28:04 +0530

    constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class complex{
+int a,b;
+//this  is  a constructor predeclaration  in  c++ programming 
+complex(int)
+{
+cout<<  "hi im  a consructor";
+}
+}
+
+int main()
+{
+complex o1,o2,o3;
+
+}
